9f677ed729ab55194216c35032bd8f6e9bfb5583
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2023-09-11 16:41:03 +0200

    evalution

--- src/main/java/evalution/InterestingneessMain.java
+++ src/main/java/evalution/InterestingneessMain.java
@@ -1,3 +1,5 @@
+package evalution;
+
 
 import java.io.File;
 import static java.lang.System.exit;
@@ -13,37 +15,57 @@ import parser.Grammar;
 import parser.GrammarFactory;
 import utils.Dictionary;
 import utils.StringModifier;
+import utils.StringModifier;
 import utils.csv.CsvFile;
 import utils.csv.CsvUtils;
 
 @NoArgsConstructor
-public class QueGG {
+public class InterestingneessMain {
 
     //private static String inputDir = "grammarFiles/en/";
-    private static String inputDir ="result/en/";
+    private static String inputDir ="/media/elahi/Elements/BachelorThesis/results_csv/";
     private static String classFileName = "src/main/resources/LexicalEntryForClass.csv";
     //private static String grammarFileName = "grammar_FULL_DATASET_EN.json";
     private static Boolean entityRetriveOnline = true;
     private static Integer numberOfEntities = -1;
-
+    
     public static void main(String[] args) throws Exception {
-        Boolean parseFlag=false,evaluationFlag=true;
-        args = new String[]{"de", "grammarFiles/de/grammar_FULL_DATASET_DE.json","grammarFiles/de/input-QALD9-train-inductive.csv"};
-        //System.out.println(inputDir+"grammar_FULL_DATASET_EN_LAST_Test.json");
+        args = new String[]{"de", "grammar_FULL_DATASET_DE.json", "grammarFiles/de/input-QALD9-train-inductive.csv"};
         if (args.length < 3) {
             System.err.printf("Too few parameters (%s/%s)", args.length);
             throw new IllegalArgumentException(String.format("Too few parameters (%s/%s)", args.length));
         }
-        String language = args[0];
-        String grammarFileName=args[1];
-        String inputFileName = args[2];
+        String language=args[0];
+        String inputFileName=args[1];
+        String[] dirs = new File(inputDir).list();
+        String[] folders = new String[]{"Conviction","Cosine","Leverage","Lift"};
+
+        for (String dir : dirs) {
+            for (String folder : folders) {
+                if (dir.contains(folder)) {
+                    String grammarFileName=inputDir+dir+File.separator+"grammar_FULL_DATASET_DE.json";
+                    String outputFileName= inputFileName.replace("input", folder+"-output");
+                    runParse(language,grammarFileName,inputFileName,outputFileName);
+                    break;
+                }
+            }
+        }
+        /*String language = "de";
+        String grammarFile = grammarDir + "grammar_FULL_DATASET_DE.json";
+        String inputFileName = "grammarFiles/de/input-QALD9-train-inductive.csv";*/
+
+    }
+
+    public static void runParse(String language,String grammarFileName,String inputFileName,String outputFileName) throws Exception {
+        Boolean parseFlag=false,evaluationFlag=true;
+        //System.out.println(inputDir+"grammar_FULL_DATASET_EN_LAST_Test.json");
         Grammar grammar = loadGrammar(grammarFileName,language,classFileName);
-        
+        File outputFile = new File(outputFileName);
         String[] header = new String[]{"ID", "status", "sentence", "sparqlQald"};
         System.out.println("Grammar Parser!!!");
         File file = new File(inputFileName);
         List<String[]> rows = CsvUtils.readAllDataAtOnce(file);
-        File outputFile = new File(inputFileName.replace("input", "output"));
+       
         List<String[]> outputs = new ArrayList<String[]>();
         outputs.add(header);
         Integer index=0;
@@ -81,7 +103,7 @@ public class QueGG {
         try {
             CsvUtils.writeDataAtOnce(outputFile, outputs);
         } catch (Exception ex) {
-            Logger.getLogger(QueGG.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InterestingneessMain.class.getName()).log(Level.SEVERE, null, ex);
             ex.getMessage();
         }
 
@@ -100,7 +122,7 @@ public class QueGG {
                 return new String[]{id, "N", sentence, "N"};
             }
         } catch (Exception ex) {
-            Logger.getLogger(QueGG.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InterestingneessMain.class.getName()).log(Level.SEVERE, null, ex);
             throw new Exception("Parsing failed for the text:"+sentence);
 
         }
@@ -120,7 +142,7 @@ public class QueGG {
                 return new String[]{id, "N", sentence, givenSparql,"N"};
             }
         } catch (Exception ex) {
-            Logger.getLogger(QueGG.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InterestingneessMain.class.getName()).log(Level.SEVERE, null, ex);
             throw new Exception("Parsing failed for the text:" + sentence);
 
         }
@@ -130,7 +152,7 @@ public class QueGG {
          try {
             return new GrammarFactory(new File(grammarFileName), entityRetriveOnline, numberOfEntities, language,classFileName).getGrammar();
         } catch (Exception ex) {
-            Logger.getLogger(QueGG.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InterestingneessMain.class.getName()).log(Level.SEVERE, null, ex);
             throw new Exception("Grammar loading fail!!!!");
         }
         
